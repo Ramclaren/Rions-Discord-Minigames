54276b9bf12e275e0e46757e3ce3d355657ecd83
Rion McLaren <89875257+Ramclaren@users.noreply.github.com>
2022-10-24 18:29:02 -0400

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,39 @@
 const { REST, SlashCommandBuilder, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
 
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+/*
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
@@ -12,21 +45,10 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-/*
-// for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
-
-// for global commands
-rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-	.then(() => console.log('Successfully deleted application command'))
-	.catch(console.error);
-*/
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
-
+*/
 	
 
     
